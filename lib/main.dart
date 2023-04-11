23c0e4c2b24992588e1fbc69ddf6873e9c7bb3a6
SandroRF <sandrorf1973@gmail.com>
2023-04-11 11:03:23 -0300

    Aula - 203

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:minhas_viagens/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
